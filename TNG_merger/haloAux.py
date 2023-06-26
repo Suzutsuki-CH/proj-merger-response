ca18318303bfdb0b54e80ed5ca16d06d5b131d0f
Chi Han <han_chi0727@outlook.com>
2023-06-26 12:35:44 -0400

    period correction done

--- TNG_merger/haloAux.py
+++ TNG_merger/haloAux.py
@@ -1,36 +1,44 @@
 import illustris_python as il
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import pandas as pd
 import os
 from matplotlib import cm
 import tqdm
 
-class HaloObj: # Class of Halo Objects
-    
+
+class HaloObj:  # Class of Halo Objects
     def __init__(self, hID, sID, bPATH, mkdir=False):
-        """
-        """
+        """ """
         self.haloID = hID
         self.snapshotID = sID
         self.basePath = bPATH
 
-        fields = ['Masses','Coordinates','Velocities']
-        self.gas = il.snapshot.loadHalo(self.basePath, self.snapshotID, self.haloID, 'gas', fields=fields)
+        fields = ["Masses", "Coordinates", "Velocities"]
+        self.gas = il.snapshot.loadHalo(
+            self.basePath, self.snapshotID, self.haloID, "gas", fields=fields
+        )
 
-        fields = ['Coordinates','Velocities']
-        self.DM = il.snapshot.loadHalo(self.basePath, self.snapshotID, self.haloID, 'dm', fields=fields)
+        fields = ["Coordinates", "Velocities"]
+        self.DM = il.snapshot.loadHalo(
+            self.basePath, self.snapshotID, self.haloID, "dm", fields=fields
+        )
 
-        fields = ['Masses','Coordinates','Velocities']
-        self.star = il.snapshot.loadHalo(self.basePath, self.snapshotID, self.haloID, 'star', fields=fields)
+        fields = ["Masses", "Coordinates", "Velocities"]
+        self.star = il.snapshot.loadHalo(
+            self.basePath, self.snapshotID, self.haloID, "star", fields=fields
+        )
 
-        halo = il.groupcat.loadSingle(self.basePath, self.snapshotID, haloID=self.haloID)
-        self.CMV = halo['GroupVel']
-        self.CM = halo['GroupCM']
+        halo = il.groupcat.loadSingle(
+            self.basePath, self.snapshotID, haloID=self.haloID
+        )
+        self.CMV = halo["GroupVel"]
+        self.CM = halo["GroupCM"]
 
-        self.R200 = halo['Group_R_TopHat200']  
+        self.R200 = halo["Group_R_TopHat200"]
         self.mkdir = mkdir
-        
+
         if self.mkdir == True:
             target_PATH = "s%s_h%s" % (self.snapshotID, self.haloID)
             isExist = os.path.exists(target_PATH)
@@ -38,31 +46,73 @@ class HaloObj: # Class of Halo Objects
             if not isExist:
                 os.makedirs(target_PATH)
                 print("New directory made")
-    
 
-    def calculate_relative(self, particleType: str):
+    def period_correction(self, particleType: str):
+        # This correction work only for TNG100-1 data,
+        # not really sure what is the period size for other simulation
+        def correction(coord):
+            return ((coord + 37500) % 75000) - 37500
 
         if particleType == "DM":
-            relaV = self.DM['Velocities'] - self.CMV
-            relaR = self.DM['Coordinates'] - self.CM
+            Rx = self.DM["Coordinates"][:, 0]
+            Ry = self.DM["Coordinates"][:, 1]
+            Rz = self.DM["Coordinates"][:, 2]
+            if (Rx.max() - Rx.min()) > 70000:
+                Rx = correction(Rx)
+            if (Ry.max() - Ry.min()) > 70000:
+                Ry = correction(Ry)
+            if (Rz.max() - Rz.min()) > 70000:
+                Rz = correction(Rz)
+            self.DM["Coordinates"] = np.array([Rx, Ry, Rz]).T
 
-            return relaV, relaR
-        
         elif particleType == "gas":
-            relaV = self.gas['Velocities'] - self.CMV
-            relaR = self.gas['Coordinates'] - self.CM
+            Rx = self.gas["Coordinates"][:, 0]
+            Ry = self.gas["Coordinates"][:, 1]
+            Rz = self.gas["Coordinates"][:, 2]
+            if (Rx.max() - Rx.min()) > 70000:
+                Rx = correction(Rx)
+            if (Ry.max() - Ry.min()) > 70000:
+                Ry = correction(Ry)
+            if (Rz.max() - Rz.min()) > 70000:
+                Rz = correction(Rz)
+            self.gas["Coordinates"] = np.array([Rx, Ry, Rz]).T
 
-            return relaV, relaR
-        
         elif particleType == "star":
-            relaV = self.star['Velocities'] - self.CMV
-            relaR = self.star['Coordinates'] - self.CM
+            Rx = self.star["Coordinates"][:, 0]
+            Ry = self.star["Coordinates"][:, 1]
+            Rz = self.star["Coordinates"][:, 2]
+            if (Rx.max() - Rx.min()) > 70000:
+                Rx = correction(Rx)
+            if (Ry.max() - Ry.min()) > 70000:
+                Ry = correction(Ry)
+            if (Rz.max() - Rz.min()) > 70000:
+                Rz = correction(Rz)
+            self.star["Coordinates"] = np.array([Rx, Ry, Rz]).T
+
+        else:
+            print("No such particle")
+
+    def calculate_relative(self, particleType: str):
+        if particleType == "DM":
+            relaV = self.DM["Velocities"] - self.CMV
+            relaR = self.DM["Coordinates"] - self.CM
 
             return relaV, relaR
-        
-        else:
-            print('No such particle')
 
+        elif particleType == "gas":
+            relaV = self.gas["Velocities"] - self.CMV
+            relaR = self.gas["Coordinates"] - self.CM
+
+            return relaV, relaR
+
+        elif particleType == "star":
+            relaV = self.star["Velocities"] - self.CMV
+            relaR = self.star["Coordinates"] - self.CM
+
+            return relaV, relaR
+
+        else:
+            print("No such particle")
 
     def claculate_radial_v(self, particleType):
         relaV, relaR = self.calculate_relative(particleType)
@@ -71,15 +121,14 @@ class HaloObj: # Class of Halo Objects
         radialv_vec = []
 
         for i, v in enumerate(relaV):
-            rv = np.inner(v, relaR[i])/np.linalg.norm(relaR[i])
+            rv = np.inner(v, relaR[i]) / np.linalg.norm(relaR[i])
             radialv.append(rv)
-            radialv_vec.append(rv * relaR[i]/np.linalg.norm(relaR[i]))
-            
+            radialv_vec.append(rv * relaR[i] / np.linalg.norm(relaR[i]))
+
         radialv = np.array(radialv)
         radialv_vec = np.array(radialv_vec)
 
         return radialv, radialv_vec
-
 
     def r_vr(self, particleType: str):
         radialv, radialv_vec = self.claculate_radial_v(particleType)
@@ -87,22 +136,35 @@ class HaloObj: # Class of Halo Objects
 
         Radius = np.linalg.norm(relaR, axis=1)
 
-        fig, ax = plt.subplots(1,2, figsize=(12,5), dpi=300, sharey=True)
+        fig, ax = plt.subplots(1, 2, figsize=(12, 5), dpi=300, sharey=True)
 
-        ax[0].hist2d(Radius/np.max(Radius), radialv, bins=150);
+        ax[0].hist2d(Radius / np.max(Radius), radialv, bins=150)
         ax[0].set_xlabel("r/r$_{max}$", fontsize=15)
         ax[0].set_ylabel("v$_r$", fontsize=17)
-        ax[0].axvline(x=self.R200/np.max(Radius),
-                    color='red',ls="--", lw=3, alpha=0.4, label="Virial Radius")
+        ax[0].axvline(
+            x=self.R200 / np.max(Radius),
+            color="red",
+            ls="--",
+            lw=3,
+            alpha=0.4,
+            label="Virial Radius",
+        )
         ax[0].legend()
 
-        ax[1].hist2d(np.log10(Radius/np.max(Radius)), radialv, bins=150);
-        ax[1].axvline(x=np.log10(self.R200/np.max(Radius)),
-                    color='red',ls="--", lw=3, alpha=0.4, label="Virial Radius")
+        ax[1].hist2d(np.log10(Radius / np.max(Radius)), radialv, bins=150)
+        ax[1].axvline(
+            x=np.log10(self.R200 / np.max(Radius)),
+            color="red",
+            ls="--",
+            lw=3,
+            alpha=0.4,
+            label="Virial Radius",
+        )
         ax[1].set_xlabel("log(r/r$_{max}$)", fontsize=15)
-        
+
+        plt.tight_layout(pad=0.0, w_pad=-0.1, h_pad=0.3)
+
         return fig, ax
-    
 
     def binning(self, particleType, bin_num, axis=2):
         relaV, relaR = self.calculate_relative(particleType)
@@ -115,34 +177,34 @@ class HaloObj: # Class of Halo Objects
         # relaV = relaV[sample_ind]
 
         if axis == 0:
-            relaR = np.array([relaR[:,1], relaR[:,2]]).T
+            relaR = np.array([relaR[:, 1], relaR[:, 2]]).T
             area = np.array([area[1], area[2]])
             initialR = np.array(min_values[1], min_values[2])
         elif axis == 1:
-            relaR = np.array([relaR[:,0], relaR[:,2]]).T
+            relaR = np.array([relaR[:, 0], relaR[:, 2]]).T
             area = np.array([area[0], area[2]])
             initialR = np.array(min_values[0], min_values[2])
         elif axis == 2:
-            relaR = np.array([relaR[:,0], relaR[:,1]]).T
+            relaR = np.array([relaR[:, 0], relaR[:, 1]]).T
             area = np.array([area[0], area[1]])
             initialR = np.array(min_values[0], min_values[1])
-        else: 
+        else:
             pass
 
         binsize = area / bin_num
         print(binsize)
         print(relaR.shape)
-        
+
         vert_velo = []
         centerP = []
         meanvelo = []
 
         for i in tqdm.tqdm(range(bin_num[0])):
             for j in range(bin_num[1]):
-                mm = initialR + np.array([i*binsize[0], j*binsize[1]])
-                MM = initialR + np.array([(i+1)*binsize[0], (j+1)*binsize[1]])
-                centerP.append((mm+MM)/2)
-                
+                mm = initialR + np.array([i * binsize[0], j * binsize[1]])
+                MM = initialR + np.array([(i + 1) * binsize[0], (j + 1) * binsize[1]])
+                centerP.append((mm + MM) / 2)
+
                 mask = np.all((relaR >= mm) & (relaR < MM), axis=1)
                 particle_in_bin = relaV[mask]
                 # print(len(particle_in_bin))
@@ -158,44 +220,48 @@ class HaloObj: # Class of Halo Objects
                 elif axis == 2:
                     meanvelo.append([binned_V[0], binned_V[1]])
                     vert_velo.append(binned_V[2])
-                    
-                else: pass
+
+                else:
+                    pass
 
         centerP = np.array(centerP)
         meanvelo = np.array(meanvelo)
         vert_velo = np.array(vert_velo)
 
-
         return centerP, meanvelo, vert_velo, binsize
-    
 
     def veloPlot(self, particleType, bin_num, axis=2):
         c, mv, vv, bs = self.binning(particleType, bin_num)
-        
+
         norm_factor = np.max(bs)
         limC = np.max(np.abs(c)) + norm_factor
 
-        mv_normed = mv/np.nanmax(np.linalg.norm(mv, axis=1)) * norm_factor * 1.5
-        
-        cw = cm.get_cmap('coolwarm', 24)
-        vv_color = (vv - np.nanmin(vv))/np.nanmax(vv-np.nanmin(vv))
+        mv_normed = mv / np.nanmax(np.linalg.norm(mv, axis=1)) * norm_factor * 1.5
 
-        plt.figure(figsize=(10,10), dpi=300)
+        cw = cm.get_cmap("coolwarm", 24)
+        vv_color = (vv - np.nanmin(vv)) / np.nanmax(vv - np.nanmin(vv))
+
+        plt.figure(figsize=(10, 10), dpi=300)
         ax = plt.gca()
         ax.set_facecolor("silver")
         for i, p in tqdm.tqdm(enumerate(c)):
-            x = (p - mv_normed[i]/2)[0]
-            y = (p - mv_normed[i]/2)[1]
+            x = (p - mv_normed[i] / 2)[0]
+            y = (p - mv_normed[i] / 2)[1]
             dx = mv_normed[i][0]
             dy = mv_normed[i][1]
-            #print(x,y,dx,dy)
+            # print(x,y,dx,dy)
             ax.set_xlim(-limC, limC)
             ax.set_ylim(-limC, limC)
-            #plt.quiver(x,y,dx,dy, color="black", alpha=0.7);
-            ax.plot([x,x+dx], [y,y+dy], color=cw(vv_color)[i], lw=1.05)
-            ax.scatter(x+dx, y+dy, marker=".", s=15, color="black")
+            # plt.quiver(x,y,dx,dy, color="black", alpha=0.7);
+            ax.plot([x, x + dx], [y, y + dy], color=cw(vv_color)[i], lw=1.05)
+            ax.scatter(x + dx, y + dy, marker=".", s=15, color="black")
             ax.grid()
 
-        ax.set_title("S%s H%s, axis=%s, particle=%s" % (self.snapshotID, self.haloID, axis, particleType) , fontsize=20)
-        ax.set_xlabel('$\Delta$x [ckpc/h]', fontsize = 15)
-        ax.set_ylabel('$\Delta$y [ckpc/h]', fontsize = 15)
+        ax.set_title(
+            "S%s H%s, axis=%s, particle=%s"
+            % (self.snapshotID, self.haloID, axis, particleType),
+            fontsize=20,
+        )
+        ax.set_xlabel("$\Delta$x [ckpc/h]", fontsize=15)
+        ax.set_ylabel("$\Delta$y [ckpc/h]", fontsize=15)
+
